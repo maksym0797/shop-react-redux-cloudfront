e79f980faa84e5e08af960c4536188d3c7c694fe
maksym0797 <maaaaksiiiim@gmail.com>
2022-08-01 12:59:53 +0300

    add task 5 integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://32qsgp54nc.execute-api.eu-central-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://8c730cgt0b.execute-api.eu-central-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
